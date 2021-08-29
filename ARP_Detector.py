b18ee757b6a78124969bf4937d9f7bd250098273
Yousef Adel <51286063+Anonymous2003@users.noreply.github.com>
2021-08-29 23:49:43 +0300

    Update ARP_Detector.py

--- ARP_Detector.py
+++ ARP_Detector.py
@@ -1,12 +1,34 @@
-import scapy.all as scapy
+from termcolor import colored
+import os
+import winsound
+import pyfiglet
+def get_mac(ip):
+    arp = scapy.ARP(pdst = ip)
+    broadcast = scapy.Ether(dst = 'ff:ff:ff:ff:ff:ff')
+    packet = broadcast/arp
+    answered = scapy.srp(packet , timeout = 1 , verbose = False)[0]
+    return answered[0][1].hwsrc
 
 
-def sniff(interface):
-    scapy.sniff(iface = interface , store = False , prn = packet_sniifer)
+def sniff():
+    scapy.sniff( store = False , prn = packet_sniifer)
+    # we used scapy sniff function inorder to sniff on traffic and prn to recall our function
 
 def packet_sniifer(packet):
     if packet.haslayer(scapy.ARP)and packet[scapy.ARP].op == 2 :
-        print(packet.show())
+    # here we used .haslayer to check for ARP packets and ARP layer then print them in deatials using .show() function
+        try:
+            real_mac = get_mac(packet[scapy.ARP].psrc)
+            response_mac = packet[scapy.ARP].hwsrc
+            if real_mac != response_mac :
+                winsound.Beep(500, 1000)
+                print(colored("[+] You are Under Attack", "red" , attrs=["bold"]))
+        except IndexError:
+            pass
 
-
+        #print(packet.show())
+os.system("cls")
+text = pyfiglet.figlet_format("MIMT-Detect")
+text_color = colored(text , "cyan" , attrs=["bold"])
+print(text_color)
 sniff()
